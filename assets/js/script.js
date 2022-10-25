3d212f211cad45b9349bb3f6fa0196a29a0ea9c6
maciiv <48811369+maciiv@users.noreply.github.com>
2022-10-25 22:38:06 +1000

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,14 @@
 const portfolioItems = document.querySelectorAll(".portfolio-item")
 const portfolioItemsHover = function(e) {
     let itemInfo = e.target.querySelector(".portfolio-info")
-    console.log(itemInfo.style.width)
-    itemInfo.style.width = e.target.getBoundingClientRect().width
+    if(itemInfo === null) return
+    itemInfo.style.width = `${e.target.querySelector("img").getBoundingClientRect().width - 30}px`
+    itemInfo.style.marginLeft = "15px"
 }
 portfolioItems.forEach((pi) => pi.addEventListener("mouseover", portfolioItemsHover))
+
+const portfolioFilterButtons = document.querySelectorAll("#portfolio-flters li")
+const filterPortfolioItems = function(filter) {
+    portfolioItems.forEach((pi) => pi.classList.toggle("d-none", pi.dataset.filter !== filter))
+}
+portfolioFilterButtons.forEach((fb) => fb.addEventListener("click", ))
