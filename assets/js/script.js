fe1cff6eead618532c3d381116065ec8d1b4c8b3
maciiv <48811369+maciiv@users.noreply.github.com>
2022-10-25 21:17:39 +1000

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 const portfolioItems = document.querySelectorAll(".portfolio-item")
 const portfolioItemsHover = function(e) {
     let itemInfo = e.target.querySelector(".portfolio-info")
-    itemInfo.style.width = e.target.getRoundClient().width
+    console.log(itemInfo.style.width)
+    itemInfo.style.width = e.target.getBoundingClientRect().width
 }
 portfolioItems.forEach((pi) => pi.addEventListener("mouseover", portfolioItemsHover))
