c5ed4577d37984f4497e1f336073be13513f6b0f
Miguel Canizares <48811369+maciiv@users.noreply.github.com>
2022-10-27 16:47:49 +1000

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
-const navbarLink = document.querySelectorAll("#header .nav-link")
-navbarLink.forEach((nl) => nl.addEventListener("click", function(e) {
+// Change active class in navbar links
+const navLinks = document.querySelectorAll("#header .nav-link")
+const toggleNavLinks = (e) => {
     document.querySelector("#header .nav-link.active").classList.remove("active")
     e.target.classList.add("active")
-}))
+}
+navLinks.forEach((nl) => nl.addEventListener("click", toggleNavLinks))
 
+// Filter portfolio items
 const portfolioItems = document.querySelectorAll(".portfolio-item")
 const portfolioFilterButtons = document.querySelectorAll("#portfolio-flters li")
-const filterPortfolioItems = function(e) {
+const filterPortfolioItems = (e) => {
     portfolioItems.forEach((pi) => pi.classList.toggle("d-none", e.target.dataset.filter !== "all" ? pi.dataset.item !== e.target.dataset.filter : false))
 }
 portfolioFilterButtons.forEach((fb) => fb.addEventListener("click", filterPortfolioItems))
