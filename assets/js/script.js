a0b947d571c555ca8ab2081ea49ee88bfd6943fc
maciiv <48811369+maciiv@users.noreply.github.com>
2022-11-16 15:09:15 +1000

    update script

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,6 @@ const portfolioFilterButtons = document.querySelectorAll("#portfolio-filters li"
 const filterPortfolioItems = (e) => {
     document.querySelector("#portfolio-filters .active").classList.remove("active")
     e.target.classList.add("active")
-    portfolioItems.forEach((pi) => pi.classList.toggle("d-none", e.target.dataset.filter !== "all" ? pi.dataset.item !== e.target.dataset.filter : false))
+    portfolioItems.forEach((pi) => pi.parentElement.classList.toggle("d-none", e.target.dataset.filter !== "all" ? pi.dataset.item !== e.target.dataset.filter : false))
 }
 portfolioFilterButtons.forEach((fb) => fb.addEventListener("click", filterPortfolioItems))
